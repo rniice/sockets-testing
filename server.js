5dffc2201ac4f6e41a00913efbf770246d20b05e
Michael Crockett <michael.crockett@gmail.com>
2016-05-24 00:34:26 -0700

    initial test between client browser and nodejs server

---
+++ server.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 80;
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+	console.log("sending index.html");
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+
+  socket.on('client response', function (data) {
+  	if(data.my === 'foo') {
+  		console.log('received foo!');
+	  	socket.emit('server response', { my: 'bar' });
+  	}
+  	else if(data.my === 'foobar'){
+  		console.log('received foobar!')
+  		socket.emit('server response', { my: 'complete' });
+  	}
+
+  	else{
+  		console.log('did not receive foo, or foobar = (')
+  	}
+  });
+
+  socket.on('disconnect', function(data){
+  	console.log('client disconnected');
+  });
+
+
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+
+});
